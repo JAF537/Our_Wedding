d5edfd8048265284d7e651396e31cabb1e1e1080
JAF537 <jaflor70@gmail.com>
2025-02-23 15:42:21 +0100

    Add files via upload

--- Our_Wedding/script.js
+++ Our_Wedding/script.js
@@ -1,3 +1,14 @@
-function openEnvelope() {
-    document.getElementById('scroll').style.display = 'block';
+function abrirSobre() {
+    let pergamino = document.getElementById('pergamino');
+    pergamino.style.display = 'block';
+    pergamino.style.animation = 'desenrollar 3s ease-out forwards';
 }
+
+setInterval(() => {
+    let slides = document.querySelectorAll('.slide');
+    let active = document.querySelector('.slide:not([style*="display: none"])');
+    let next = active.nextElementSibling || slides[0];
+    
+    active.style.display = 'none';
+    next.style.display = 'block';
+}, 3000);
